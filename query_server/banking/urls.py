527122d91d369b8e0e08d129c9d5989876b9f461
“adenHustlin” <baekdam8@gmail.com>
2024-05-20 22:55:36 +0900

    feat: 조회서버 폴더명 변경
    - app 이름변경

--- query_server/banking/urls.py
+++ query_server/banking/urls.py
@@ -4,7 +4,7 @@ from rest_framework.routers import DefaultRouter
 from .views import TransactionViewSet
 
 router = DefaultRouter()
-router.register(r"transactions", TransactionViewSet, basename="transaction")
+router.register(r"banking", TransactionViewSet, basename="transaction")
 
 urlpatterns = [
     path("", include(router.urls)),
